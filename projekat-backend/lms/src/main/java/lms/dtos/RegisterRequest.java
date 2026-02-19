f8ae36a92117a58b9e19a77bdb2b2a01c34beffe
marko924 <medamarko2003@gmail.com>
2026-02-19 18:52:39 +0100

    Adding necessary dtos for autentification and registration

---
+++ projekat-backend/lms/src/main/java/lms/dtos/RegisterRequest.java
@@ -0,0 +1,85 @@
+package lms.dtos;
+
+public class RegisterRequest {
+	
+	private String korisnickoIme;
+    private String lozinka;
+    private String email;
+    private String uloga;
+
+    // Specifična polja (biće null ako se ne pošalju):
+    
+    private String jmbg; // Za studenta
+    private String ime;
+    private String prezime;
+    
+    private String biografija;      // Za nastavnika
+
+    public RegisterRequest() {}
+
+	public String getKorisnickoIme() {
+		return korisnickoIme;
+	}
+
+	public void setKorisnickoIme(String korisnickoIme) {
+		this.korisnickoIme = korisnickoIme;
+	}
+
+	public String getLozinka() {
+		return lozinka;
+	}
+
+	public void setLozinka(String lozinka) {
+		this.lozinka = lozinka;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUloga() {
+		return uloga;
+	}
+
+	public void setUloga(String uloga) {
+		this.uloga = uloga;
+	}
+
+	public String getJmbg() {
+		return jmbg;
+	}
+
+	public void setJmbg(String jmbg) {
+		this.jmbg = jmbg;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+
+	public String getBiografija() {
+		return biografija;
+	}
+
+	public void setBiografija(String biografija) {
+		this.biografija = biografija;
+	}
+    
+    
+}
